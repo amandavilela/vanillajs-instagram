80461b4b374dd646dd0dc46a91700c9c4d781d9d
amanda <amandavilelaalmeida@gmail.com>
2017-09-18 23:38:52 -0300

    Create a promise to obtain data via http requistions

--- js/main.js
+++ js/main.js
@@ -5,12 +5,15 @@ import http from './http.js';
 const user = document.getElementById('user');
 const listPosts = document.getElementById('list-posts');
 
-http('http://www.mocky.io/v2/59bb3b940f0000c007622abb', 'GET')
-  .then(function(data){
+async function getData() {
+  return await http('http://www.mocky.io/v2/59bb3b940f0000c007622abb', 'GET');
+}
+
+getData().then(function(data){
     data = JSON.parse(data);
     renderUserInfo(data, user);
     renderUserPosts(data.posts, listPosts);
   })
   .catch(function(err){
-    console.log('err: ', err)
+    console.log('err: ', err);
   });
